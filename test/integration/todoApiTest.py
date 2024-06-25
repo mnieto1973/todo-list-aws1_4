e9e8daac21ec8fbaa6eaa8d339ba94dccf64d55b
Mariano Nieto Viñas <mnieto73@hotmail.com>
2024-06-25 12:29:26 +0200

    Cambios en los test de integracion para solo ejecutar en produccion los de lectura

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -18,7 +18,7 @@ class TestApi(unittest.TestCase):
     def setUp(self):
         self.assertIsNotNone(BASE_URL, "URL no configurada")
         self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
-
+    @pytest.mark.testProduccion
     def test_api_listtodos(self):
         
         print('---------------------------------------')
@@ -50,6 +50,7 @@ class TestApi(unittest.TestCase):
         self.assertTrue(response.json())
         
         print('End - integration test List TODO')
+    @pytest.mark.testStaging
     def test_api_addtodo(self):
         print('---------------------------------------')
         print('Starting - integration test Add TODO')
@@ -75,6 +76,7 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Add TODO')
+    @pytest.mark.testProduccion
     def test_api_gettodo(self):
         print('---------------------------------------')
         print('Starting - integration test Get TODO')
@@ -112,7 +114,7 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Get TODO')
-    
+    @pytest.mark.testStaging
     def test_api_updatetodo(self):
         print('---------------------------------------')
         print('Starting - integration test Update TODO')
@@ -166,6 +168,7 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Update TODO')
+    @pytest.mark.testStaging
     def test_api_deletetodo(self):
         print('---------------------------------------')
         print('Starting - integration test Delete TODO')
